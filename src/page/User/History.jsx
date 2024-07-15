fbfac8402d73d4582f436eba561158c55ad7272e
owfej213 <wljurse@gmail.com>
2024-07-15 17:59:04 +0800

    history page

--- src/page/User/History.jsx
+++ src/page/User/History.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Wrapper from "../../components/ui/Wrapper";
 import {
   Box,
+  Button,
   Checkbox,
   Flex,
   Grid,
@@ -17,6 +18,7 @@ import {
   Thead,
   Tr,
   VStack,
+  useTheme,
 } from "@chakra-ui/react";
 import { useMailsData } from "../../components/Context/MailsDataContext";
 import { formateDateYMD } from "../../utils/dateUtils";
@@ -25,13 +27,16 @@ import Container from "../../components/ui/Container";
 import SubTitle from "../../components/ui/SubTitle";
 import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import Icon from "../../components/ui/Icon";
+import { db } from "../../firebase/firebase";
+import { collection, deleteDoc, doc } from "firebase/firestore";
 
 const MotionBox = motion(Box);
 function TableRow({ children, ...props }) {
   return (
     <>
       <Flex
-        maxW="1000px"
+        maxW="1200px"
         w="100%"
         direction={["column", "column", "row"]}
         {...props}
@@ -74,6 +79,21 @@ TableItem.propTypes = {
 };
 
 function List({ filteredMails, userData }) {
+  const theme = useTheme();
+  const { mailBoxID } = userData || {};
+
+  const handleDeleteMail = async (uid) => {
+    try {
+      const result = doc(
+        collection(doc(db, `mailBoxes/${mailBoxID}`), "mails"),
+        uid
+      );
+      await deleteDoc(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       {filteredMails &&
@@ -82,62 +102,66 @@ function List({ filteredMails, userData }) {
           if (!mail?.createAt) return;
 
           let TableRowColor = "";
+          let initialTableRowColor = "";
+          let currentTime = new Date();
+
+          if (currentTime.getTime() / 1000 - mail.createAt.seconds < 60) {
+            initialTableRowColor = theme.colors.red[500];
+          } else {
+            initialTableRowColor = theme.colors.gray[200];
+          }
 
           if (mail.receiver === userData.userRealName) {
-            TableRowColor = "cyan.100";
+            TableRowColor = theme.colors.green[200];
           } else {
-            TableRowColor = "gray.300";
+            TableRowColor = theme.colors.gray[200];
           }
 
           return (
             <Box key={mail.createAt.seconds} w="100%">
               <AnimatePresence>
                 <MotionBox
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
+                  initial={{
+                    opacity: 0,
+                    backgroundColor: initialTableRowColor,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    backgroundColor: TableRowColor,
+                  }}
                   exit={{ opacity: 0 }}
                   transition={{ duration: 0.5 }}
                 >
-                  {/* <Show above="md"> */}
-                  <TableRow bg={TableRowColor}>
+                  <TableRow>
                     <TableItem maxW="300px">
                       {formateDateYMD(mail.createAt.seconds)}
                     </TableItem>
                     <TableItem maxW="250px">{mail.title}</TableItem>
-                    <TableItem maxW="150px">{mail.receiver}</TableItem>
+                    <TableItem maxW="200px">{mail.receiver}</TableItem>
                     <TableItem maxW="150px">{mail.urgency}</TableItem>
-                    <TableItem
-                      maxW="150px"
-                      fontWeight="bold"
-                      color="teal.600"
-                      _hover={{
-                        color: "teal.400",
-                      }}
-                    >
-                      <Link to={`/detail/${mail.uid}`}>查看</Link>
+                    <TableItem maxW="150px">
+                      <Link to={`/detail/${mail.uid}`}>
+                        <Text
+                          fontWeight="bold"
+                          color="teal.600"
+                          _hover={{
+                            color: "teal.400",
+                          }}
+                        >
+                          查看
+                        </Text>
+                      </Link>
+                    </TableItem>
+                    <TableItem maxW="150px" align="center">
+                      <Button
+                        p="0"
+                        onClick={() => handleDeleteMail(mail.uid)}
+                        bg="transparent"
+                      >
+                        <Icon name="Trash2" color="red" size="32" />
+                      </Button>
                     </TableItem>
                   </TableRow>
-                  {/* </Show> */}
-                  {/* <Hide above="md">
-                    <TableRow bg={TableRowColor}>
-                      <TableItem title="收件日期">
-                        {formateDateYMD(mail.createAt.seconds)}
-                      </TableItem>
-                      <TableItem title="郵件標題">{mail.title}</TableItem>
-                      <TableItem title="收信人">{mail.receiver}</TableItem>
-                      <TableItem title="緊急度">{mail.urgency}</TableItem>
-                      <TableItem
-                        title="細節"
-                        fontWeight="bold"
-                        color="teal.600"
-                        _hover={{
-                          color: "teal.400",
-                        }}
-                      >
-                        <Link to={`/detail/${mail.uid}`}>查看</Link>
-                      </TableItem>
-                    </TableRow>
-                  </Hide> */}
                 </MotionBox>
               </AnimatePresence>
             </Box>
@@ -177,7 +201,8 @@ function History() {
         return result;
       });
     }
-  }, [filteredMails, mailsData, myMailsCheckbox, userData]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mailsData, myMailsCheckbox]);
   return (
     <>
       <MotionBox
@@ -187,15 +212,16 @@ function History() {
         transition={{ duration: 0.5 }}
       >
         <Wrapper>
-          <VStack spacing="1" maxW="1000px" w="100%">
+          <VStack spacing="1" maxW="1200px" w="100%">
             <Grid
-              maxW="1000px"
+              maxW="1200px"
               w="100%"
               templateColumns="repeat(8, 1fr)"
               gap={4}
+              mb="4"
             >
               <GridItem colSpan={[8, 8, 4]}>
-                <Container mb="4">
+                <Container>
                   <SubTitle>說明</SubTitle>
                   <TableContainer>
                     <Table variant="simple">
@@ -228,27 +254,28 @@ function History() {
                 </Container>
               </GridItem>
               <GridItem colSpan={[8, 8, 4]}>
-                <Container mb="8">
-                  {" "}
+                <Container h="100%">
                   <SubTitle>設定</SubTitle>
-                  <Checkbox
-                    onChange={(e) => {
-                      setMyMailsCheckbox(e.target.checked);
-                    }}
-                  >
-                    顯示本人郵件
-                  </Checkbox>
+                  <Box h="100%">
+                    <Checkbox
+                      onChange={(e) => {
+                        setMyMailsCheckbox(e.target.checked);
+                      }}
+                    >
+                      顯示本人郵件
+                    </Checkbox>
+                  </Box>
                 </Container>
               </GridItem>
             </Grid>
-
             <Show above="md">
               <TableRow bg="gray.400">
                 <TableItem maxW="300px" title="收件日期" />
                 <TableItem maxW="250px" title="郵件標題" />
-                <TableItem maxW="150px" title="收件人" />
+                <TableItem maxW="200px" title="收件人" />
                 <TableItem maxW="150px" title="緊急度" />
                 <TableItem maxW="150px" title="細節" />
+                <TableItem maxW="150px" title="刪除" />
               </TableRow>
             </Show>
             <List filteredMails={filteredMails} userData={userData} />
